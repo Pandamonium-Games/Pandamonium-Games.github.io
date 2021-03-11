def36cb526d168971ce5ed27a56125be3f37d5b6
Pandamonium-Games <78954752+Pandamonium-Games@users.noreply.github.com>
2021-03-11 15:23:37 +0000

    Delete site.js

--- site.js
+++
@@ -1,6 +0,0 @@
-function sendMail() {
-	var subject = document.getElementById('subject').value;
-	var body = document.getElementById('body').value;
-
-	window.open('mailto:pandamonium.devteam@gmail.com?subject=' + subject + '&body=' + body);
-}
